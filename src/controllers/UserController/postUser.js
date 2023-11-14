a4fe76e50d1f883fecb07bd71c0fa4c0a5d0a446
altair201 <wilsondavianpa1998@outlook.com>
2023-11-13 21:27:56 -0500

    update server

--- src/controllers/UserController/postUser.js
+++ src/controllers/UserController/postUser.js
@@ -4,12 +4,10 @@ const jwt = require('jsonwebtoken');
 const mailUserCreated = require('../../config/mailUserCreated');
 const { JWT_SECRET } = process.env;
 
-
 const postUser = async ({ name, surname, email, phone, password, address, typeUser }) => {
+
   if (!(name || surname || email || password)) throw Error("Required data is missing. Please provide name, surname, email, and password.")
-  if(password.length < 6 || password.length > 10) throw Error('Password must be between 6 and 10 characters in length.')
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com)$/i;
-  if (!emailRegex.test(email)) throw Error('Invalid email address.')
+  if (password.length < 6 || password.length > 10) throw Error('Password must be between 6 and 10 characters in length.')
   const hashedPassword = await bcrypt.hash(password, 10);
   const [user, created] = await User.findOrCreate({
     where: {
@@ -24,17 +22,21 @@ const postUser = async ({ name, surname, email, phone, password, address, typeUs
       address: address ? address : null
     }
   })
-  if (created){mailUserCreated(email)}
+  if (created) { 
+    mailUserCreated(email)
+  }
   if (!created) throw Error("User with the provided information already exists.")
   const cartUser = await Cart.create({});
   await user.setCart(cartUser);
-  const {id} = user.dataValues;
-  const token = jwt.sign({id}, JWT_SECRET )
+  const { id } = user.dataValues;
+  const token = jwt.sign({ id }, JWT_SECRET)
   return ({ message: `User Created: ${user.name}`, token });
 }
 
 module.exports = {
   postUser
 };
+
+
 
 
